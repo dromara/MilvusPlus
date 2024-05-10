9707c1ae216b4dd86024d6055bfcb87f6448ce19
xgc <classxgc@mima>
2024-05-10 23:32:28 +0800

    提取ConditionBuilder

--- milvus-demo/src/main/java/io/github/javpower/milvus/demo/ApplicationRunnerTest.java
+++ milvus-demo/src/main/java/io/github/javpower/milvus/demo/ApplicationRunnerTest.java
@@ -3,7 +3,9 @@ package io.github.javpower.milvus.demo;
 import com.google.common.collect.Lists;
 import io.github.javpower.milvus.demo.model.Face;
 import io.github.javpower.milvus.demo.test.FaceMilvusMapper;
-import io.github.javpower.milvus.plus.model.MilvusResp;
+import io.github.javpower.milvus.plus.model.vo.MilvusResp;
+import io.github.javpower.milvus.plus.model.vo.MilvusResultVo;
+import io.milvus.v2.service.vector.response.DeleteResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -18,10 +20,16 @@ public class ApplicationRunnerTest implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         List<Float> vector = Lists.newArrayList(0.1f,0.2f,0.3f);
-        MilvusResp<Face> resp = mapper.lambda()
-                .eq(Face::getPersonId,1l)
+
+        MilvusResp<MilvusResultVo<Face>> query =  mapper.searchWrapper()
+                .eq(Face::getPersonId, 1l)
                 .vector(vector)
                 .limit(100l)
                 .query();
+
+        MilvusResp<DeleteResp> remove= mapper.deleteWrapper()
+                .eq(Face::getPersonId, 1l)
+                .id(111)
+                .remove();
     }
 }
