0981b085daea5a5a499e57712b76bf4111061def
xgc <classxgc@mima>
2024-05-11 10:41:54 +0800

    添加测试

--- milvus-demo/src/main/java/io/github/javpower/milvus/demo/ApplicationRunnerTest.java
+++ milvus-demo/src/main/java/io/github/javpower/milvus/demo/ApplicationRunnerTest.java
@@ -6,6 +6,7 @@ import io.github.javpower.milvus.demo.test.FaceMilvusMapper;
 import io.github.javpower.milvus.plus.model.vo.MilvusResp;
 import io.github.javpower.milvus.plus.model.vo.MilvusResultVo;
 import io.milvus.v2.service.vector.response.DeleteResp;
+import io.milvus.v2.service.vector.response.UpsertResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -21,15 +22,29 @@ public class ApplicationRunnerTest implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         List<Float> vector = Lists.newArrayList(0.1f,0.2f,0.3f);
 
+        //查询
         MilvusResp<MilvusResultVo<Face>> query =  mapper.searchWrapper()
                 .eq(Face::getPersonId, 1l)
                 .vector(vector)
                 .limit(100l)
                 .query();
+        MilvusResp<List<Face>> query2 = mapper.getById(1l);
 
+
+        //删除
         MilvusResp<DeleteResp> remove= mapper.deleteWrapper()
                 .eq(Face::getPersonId, 1l)
                 .id(111)
                 .remove();
+        MilvusResp<DeleteResp> remove2 = mapper.removeById(1l);
+
+        //更新
+        Face face=new Face();
+        face.setFaceVector(vector);
+        MilvusResp<UpsertResp> update = mapper.updateWrapper()
+                .eq(Face::getPersonId, 1l)
+                .update(face);
+        face.setPersonId(1l);
+        MilvusResp<UpsertResp> update2 = mapper.updateById(face);
     }
 }
