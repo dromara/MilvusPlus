9d412ac8fa48614cbe0e4a617267db7c60482f5d
xgc <classxgc@mima>
2024-05-13 10:04:41 +0800

    更新测试用例，支持批量

--- milvus-demo/src/main/java/io/github/javpower/milvus/demo/ApplicationRunnerTest.java
+++ milvus-demo/src/main/java/io/github/javpower/milvus/demo/ApplicationRunnerTest.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import io.github.javpower.milvus.demo.model.Face;
 import io.github.javpower.milvus.demo.test.FaceMilvusMapper;
 import io.github.javpower.milvus.plus.model.vo.MilvusResp;
-import io.github.javpower.milvus.plus.model.vo.MilvusResultVo;
+import io.github.javpower.milvus.plus.model.vo.MilvusResult;
 import io.milvus.v2.service.vector.response.DeleteResp;
 import io.milvus.v2.service.vector.response.InsertResp;
 import io.milvus.v2.service.vector.response.UpsertResp;
@@ -26,7 +26,7 @@ public class ApplicationRunnerTest implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) throws InterruptedException {
+    public void run(ApplicationArguments args){
         Face face=new Face();
         List<Float> vector = new ArrayList<>();
         for (int i = 0; i < 128; i++) {
@@ -35,24 +35,34 @@ public class ApplicationRunnerTest implements ApplicationRunner {
         face.setPersonId(1l);
         face.setFaceVector(vector);
         //新增
-        MilvusResp<InsertResp> insert = mapper.insert(face); log.info("insert--{}", JSONObject.toJSONString(insert));
-        Thread.sleep(1000);
-        face.setPersonId(2l);
-        MilvusResp<InsertResp> insert2 = mapper.insert(face); log.info("insert--{}", JSONObject.toJSONString(insert2));
-
+        List<Face> faces=new ArrayList<>();
+        for (int i = 1; i < 10 ;i++){
+            Face face1=new Face();
+            face1.setPersonId(Long.valueOf(i));
+            List<Float> vector1 = new ArrayList<>();
+            for (int j = 0; j < 128; j++) {
+                vector1.add((float) (Math.random() * 100)); // 这里仅作为示例使用随机数
+            }
+            face1.setFaceVector(vector1);
+            faces.add(face1);
+        }
+        MilvusResp<InsertResp> insert = mapper.insert(faces.toArray(faces.toArray(new Face[0]))); log.info("insert--{}", JSONObject.toJSONString(insert));
         //id查询
-        MilvusResp<List<Face>> query = mapper.getById(9l);
+        MilvusResp<List<MilvusResult<Face>>> query = mapper.getById(9l);
         log.info("query--getById---{}", JSONObject.toJSONString(query));
         //向量查询
-        MilvusResp<MilvusResultVo<Face>> query1 = mapper.queryWrapper().vector(Face::getFaceVector, vector)
-                .ne(Face::getPersonId,1L)
+        MilvusResp<List<MilvusResult<Face>>> query1 = mapper.queryWrapper()
+                .vector(Face::getFaceVector, vector)
+                .ne(Face::getPersonId, 1L)
                 .topK(3)
-                .search();log.info("向量查询 query--queryWrapper---{}", JSONObject.toJSONString(query1));
+                .query();
+        log.info("向量查询 query--queryWrapper---{}", JSONObject.toJSONString(query1));
         //标量查询
-        MilvusResp<List<Face>> query4 = mapper.queryWrapper().vector(Face::getFaceVector, vector)
+        MilvusResp<List<MilvusResult<Face>>> query2 = mapper.queryWrapper()
                 .eq(Face::getPersonId, 2L)
                 .topK(3)
-                .query();log.info("标量查询   query--queryWrapper---{}", JSONObject.toJSONString(query4));
+                .query();
+        log.info("标量查询   query--queryWrapper---{}", JSONObject.toJSONString(query2));
         //更新
         vector.clear();
         for (int i = 0; i < 128; i++) {
@@ -60,11 +70,11 @@ public class ApplicationRunnerTest implements ApplicationRunner {
         }
         MilvusResp<UpsertResp> update = mapper.updateById(face);log.info("update--{}", JSONObject.toJSONString(update));
         //id查询
-        MilvusResp<List<Face>> query2 = mapper.getById(1L);log.info("query--getById---{}", JSONObject.toJSONString(query2));
+        MilvusResp<List<MilvusResult<Face>>> query3 = mapper.getById(1L);log.info("query--getById---{}", JSONObject.toJSONString(query3));
         //删除
         MilvusResp<DeleteResp> remove = mapper.removeById(1L);log.info("remove--{}", JSONObject.toJSONString(remove));
         //查询
-        MilvusResp<List<Face>> query3 = mapper.getById(1L);log.info("query--{}", JSONObject.toJSONString(query3));
+        MilvusResp<List<MilvusResult<Face>>> query4 = mapper.getById(1L);log.info("query--{}", JSONObject.toJSONString(query4));
 
     }
 }
