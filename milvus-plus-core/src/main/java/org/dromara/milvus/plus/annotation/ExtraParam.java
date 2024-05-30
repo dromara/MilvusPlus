1d95b6605325a72c9e355cf5f95e72ad1b55cf67
code2tan <495140477@qq.com>
2024-05-30 10:29:07 +0800

    docs: 优化annotation下所有注解的注释

--- milvus-plus-core/src/main/java/org/dromara/milvus/plus/annotation/ExtraParam.java
+++ milvus-plus-core/src/main/java/org/dromara/milvus/plus/annotation/ExtraParam.java
@@ -4,6 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 /**
  * @author xgc
  **/
@@ -11,5 +12,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExtraParam {
     String key();
+
     String value();
 }
--- milvus-plus-core/src/main/java/org/dromara/milvus/plus/annotation/MilvusCollection.java
+++ milvus-plus-core/src/main/java/org/dromara/milvus/plus/annotation/MilvusCollection.java
@@ -4,11 +4,15 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 /**
  * @author xgc
  **/
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MilvusCollection {
-    String name(); // 集合的名称
+    /**
+     * 集合的名称
+     */
+    String name();
 }
