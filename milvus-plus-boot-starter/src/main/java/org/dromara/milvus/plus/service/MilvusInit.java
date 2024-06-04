fb868f5b705a98b869ce7789ef93b19c5f611820
code2tan <495140477@qq.com>
2024-06-04 23:11:42 +0800

    refactor: 重命名starter配置对象
    重命名starter配置对象，并移动到config目录

--- milvus-plus-boot-starter/src/main/java/org/dromara/milvus/plus/service/MilvusInit.java
+++ milvus-plus-boot-starter/src/main/java/org/dromara/milvus/plus/service/MilvusInit.java
@@ -1,7 +1,8 @@
 package org.dromara.milvus.plus.service;
 
 import io.milvus.v2.client.MilvusClientV2;
-import org.dromara.milvus.plus.entity.MilvusProperties;
+import org.dromara.milvus.plus.config.MilvusPropertiesConfiguration;
+import org.dromara.milvus.plus.model.MilvusProperties;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,15 +15,16 @@ import javax.annotation.PreDestroy;
 public class MilvusInit extends AbstractMilvusClientBuilder {
 
     @Autowired
-    private MilvusProperties milvusProperties;
+    private MilvusPropertiesConfiguration milvusPropertiesConfiguration;
+
     private MilvusClientV2 client;
 
     // Spring会调用这个方法来初始化client
     @PostConstruct
     public void initialize() {
-        org.dromara.milvus.plus.model.MilvusProperties milvusProperties1 = new org.dromara.milvus.plus.model.MilvusProperties();
-        BeanUtils.copyProperties(milvusProperties, milvusProperties1);
-        super.setProperties(milvusProperties1);
+        MilvusProperties milvusProperties = new MilvusProperties();
+        BeanUtils.copyProperties(milvusPropertiesConfiguration, milvusProperties);
+        super.setProperties(milvusProperties);
         super.initialize();
         client = getClient();
     }
