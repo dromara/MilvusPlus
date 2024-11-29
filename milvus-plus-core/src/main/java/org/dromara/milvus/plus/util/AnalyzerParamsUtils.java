e92cc55ec1ce3e41de79838d18ed99a988bd1416
xgc <javpower@163.com>
2024-11-29 16:29:16 +0800

    fix：修复2.2.0无法使用内置分析仪的问题，并优化注解的使用

--- milvus-plus-core/src/main/java/org/dromara/milvus/plus/util/AnalyzerParamsUtils.java
+++ milvus-plus-core/src/main/java/org/dromara/milvus/plus/util/AnalyzerParamsUtils.java
@@ -1,9 +1,12 @@
 package org.dromara.milvus.plus.util;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.dromara.milvus.plus.annotation.AnalyzerParams;
-import org.dromara.milvus.plus.annotation.BuiltInFilter;
 import org.dromara.milvus.plus.annotation.CustomFilter;
+import org.dromara.milvus.plus.annotation.Filter;
+import org.dromara.milvus.plus.model.BuiltInFilterType;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,31 +18,46 @@ public class AnalyzerParamsUtils {
     public static Map<String, Object> convertToMap(AnalyzerParams analyzerParams) {
         Map<String, Object> paramsMap = new HashMap<>();
         if (analyzerParams != null) {
-            // 设置分词器
-            paramsMap.put("tokenizer", analyzerParams.tokenizer().name().toLowerCase());
-            // 处理内置过滤器
-            List<String> builtInFiltersList = new ArrayList<>();
-            for (BuiltInFilter builtInFilter : analyzerParams.builtInFilters()) {
-                builtInFiltersList.add(builtInFilter.name().name());
+            String type = analyzerParams.type();
+            if(StringUtils.isNotEmpty(type)){
+                //使用默认分析器
+                paramsMap.put("type", type);
             }
-            // 处理自定义过滤器
+            String tokenizer = analyzerParams.tokenizer();
+            if(StringUtils.isNotEmpty(tokenizer)){
+                // 设置分词器
+                paramsMap.put("tokenizer",tokenizer);
+            }
+            Filter filter = analyzerParams.filter();
+            List<String> builtInFiltersList = new ArrayList<>();
             List<Map<String, Object>> customFiltersList = new ArrayList<>();
-            for (CustomFilter customFilter : analyzerParams.customFilters()) {
-                Map<String, Object> filterMap = new HashMap<>();
-                filterMap.put("type", customFilter.type());
-                if (customFilter.max() > 0) {
-                    filterMap.put("max", customFilter.max());
+            if(filter!=null){
+                CustomFilter[] customFilters = filter.customFilters();
+                BuiltInFilterType[] builtInFilterTypes = filter.builtInFilters();
+                // 处理内置过滤器
+                for (BuiltInFilterType builtInFilterType : builtInFilterTypes) {
+                    builtInFiltersList.add(builtInFilterType.name());
                 }
-                if (customFilter.stopWords().length > 0) {
-                    filterMap.put("stopWords", new ArrayList<>(Lists.newArrayList(customFilter.stopWords())));
+                //处理自定义过滤器
+                for (CustomFilter customFilter : customFilters) {
+                    Map<String, Object> filterMap = new HashMap<>();
+                    filterMap.put("type", customFilter.type());
+                    if (customFilter.max() > 0) {
+                        filterMap.put("max", customFilter.max());
+                    }
+                    if (customFilter.stopWords().length > 0) {
+                        filterMap.put("stop_words", new ArrayList<>(Lists.newArrayList(customFilter.stopWords())));
+                    }
+                    customFiltersList.add(filterMap);
                 }
-                customFiltersList.add(filterMap);
             }
             // 合并过滤器列表
             List<Object> filters = new ArrayList<>();
             filters.addAll(builtInFiltersList);
             filters.addAll(customFiltersList);
-            paramsMap.put("filter", filters);
+            if(!CollectionUtils.isEmpty(filters)){
+                paramsMap.put("filter", filters);
+            }
         }
         return paramsMap;
     }
